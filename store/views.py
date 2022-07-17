bff89bbae19cbd9d5855f751ef0f524909389568
Anib <saliv.mah@gmail.com>
2022-07-17 21:47:34 +0545

    urls.py on store, new views on store, images and css static files load

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store/Store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/Cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/Checkout.html', context)
