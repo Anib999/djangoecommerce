38c601c3dbd52c17e2b6e087382322bb742a9828
Anib <saliv.mah@gmail.com>
2022-07-23 21:20:28 +0545

    cart total, checkout data render, checkout total, @property function for total cart, and items

--- store/models.py
+++ store/models.py
@@ -37,6 +37,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
@@ -45,6 +57,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
